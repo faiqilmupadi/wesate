6030fa1d1905ae6c5c510c88416125b6f19f2199
Azzam Saefudin <azzamsaefudin.asr@gmail.com>
2024-09-24 14:54:39 +0700

    update table

--- database/seeders/KetuaProgramStudiSeeder.php
+++ database/seeders/KetuaProgramStudiSeeder.php
@@ -1,18 +1,18 @@
 <?php
 
 namespace Database\Seeders;
-use App\Models\Kaprodi;
+use App\Models\Ketuaprogramstudi;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class KaprodiSeeder extends Seeder
+class KetuaProgramStudiSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        $kaprodi = new Kaprodi;
+        $kaprodi = new Ketuaprogramstudi;
         $kaprodi -> nidn = '123456789123456789';
         $kaprodi -> nama = 'Budi';
         $kaprodi -> email = 'budi@live.undip.ac.id';
