c1f9d45991b66781cbaa04e73583c41f05de0fda
Azzam Saefudin <azzamsaefudin.asr@gmail.com>
2024-10-14 08:46:45 +0700

    updateRuangan

--- routes/web.php
+++ routes/web.php
@@ -48,24 +48,12 @@ Route::get('pemilihanrole', function () {
     return view('user.pemilihanrole', ['title' => 'pemilihanrole']);
 })->name('pemilihanrole');
 
-Route::get('approveruang', function () {
-    return view('dekan.approveruang', ['title' => 'approveruang']);
-})->name('approveruang');
-
-// Route::get('memilihmatakuliah', function () {
-//     return view('ketuaprogramstudi.memilihmatakuliah', ['title' => 'memilihmatakuliah']);
-// })->name('memilihmatakuliah');
-
-// Route::get('penyusunanruang', function () {
-//     return view('bagianakademik.penyusunanruang', ['title' => 'penyusunanruang']);
-// })->name('penyusunanruang');
-
-Route::get('penyusunanruang', [BagianAkademikController::class, 'createPenyusunanRuang'])->name('penyusunanruang.create');
+Route::get('bagianakademik/penyusunanruang', [BagianAkademikController::class, 'createPenyusunanRuang'])->name('penyusunanruang.create');
 Route::post('penyusunanruang', [BagianAkademikController::class, 'storePengalokasianRuang'])->name('penyusunanruang.store');
-Route::get('pengalokasianruang', [BagianAkademikController::class, 'createPengalokasianRuang'])->name('pengalokasianruang.create');
+Route::get('bagianakademik/pengalokasianruang', [BagianAkademikController::class, 'createPengalokasianRuang'])->name('pengalokasianruang.create');
 Route::post('pengalokasianruang', [BagianAkademikController::class, 'storePengalokasianRuang'])->name('pengalokasianruang.store');
 
-Route::get('/dekan/approve', [DekanController::class, 'createPengajuan'])->name('dekan.approveruang');
+Route::get('/dekan/approve', [DekanController::class, 'createPengajuan'])->name('pengajuan.create');
 Route::patch('/pengajuan/update/{id}', [DekanController::class, 'updatePengajuan'])->name('pengajuan.update');
 
 Route::post('pemilihanrole', [UserController::class, 'handleRoleSelection'])->name('handleRoleSelection');
